e9af83f7499a1b43f714758ba1ee3c4671ca8f0b
Parvathy Santhosh C <parvathysanthoshc5@gmail.com>
2025-08-02 18:57:32 +0530

    Add files via upload

---
+++ gif.py
@@ -0,0 +1,7 @@
+import imageio.v3 as iio
+filenames = ['chicklet1.png', 'chicklet2.png' , 'chicklet3.png' , 'chicklet4.png']
+images = [ ]
+for filename in filenames:
+    images.append(iio.imread(filename))
+iio.imwrite('chicklets.gif',images,loop = 0,duration = 500)
+print("🎉 GIF created as chicklets.gif")
