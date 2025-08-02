40342464e61274184e7b0f6d6ced78a36a0954fc
Parvathy Santhosh C <parvathysanthoshc5@gmail.com>
2025-08-02 19:10:08 +0530

    Update gif.py

--- gif.py
+++ gif.py
@@ -3,5 +3,6 @@ filenames = ['chicklet1.png', 'chicklet2.png' , 'chicklet3.png' , 'chicklet4.png
 images = [ ]
 for filename in filenames:
     images.append(iio.imread(filename))
-iio.imwrite('chicklets.gif',images,loop = 0,duration = 500)
+iio.imwrite('chicklets.gif',images,loop = 0,duration = 100)
+
 print("🎉 GIF created as chicklets.gif")
